6ea763bf76945c96375486fc9cd76ea2abd143f1
John Gonzalez <fredysna@hotmail.com>
2021-09-07 21:30:54 -0500

    se agrega ejemplo de arrowfunction in class

--- typescriptexample.ts
+++ typescriptexample.ts
@@ -7,3 +7,18 @@ sum(10,20);
 let Print = () => console.log("Hello TypeScript");
 
 Print(); //Output: Hello TypeScript
+
+//Arrow Function in Class Copy
+class Employee {
+    empCode: number;
+    empName: string;
+
+    constructor(code: number, name: string) {
+        this.empName = name;
+        this.empCode = code;
+    }
+
+    display = () => console.log(this.empCode +' ' + this.empName)
+}
+let emp = new Employee(1, 'Ram');
+emp.display();
